f146573fc6c60b5cddb0e2a6e3f39b1aefbc6b00
JacksonDrolet <droletjj@oldcolony.info>
2026-04-09 13:47:11 -0400

    Hands On 2-1 Completion
    Completed Hands On Project 2-1

--- HandsOn1/project02-01.js
+++ HandsOn1/project02-01.js
@@ -9,12 +9,22 @@
       Filename: project02-01.js
  */
 
-window.onchange
-
-function FahrenheitToCelsius (degree){
-    return degree - 32 / 1.8;
+function CelsiusToFahrenheit() {
+    Cn=document.getElementById("cValue");
+    F = fN.value * 1.8 + 32;
+    Cn.value=F;
+    //return F;   
 }
 
-function CelsiusToFahrenheit (degree){
-    return degree * 1.8 + 32;
+fN=document.getElementById("fValue");
+fN.addEventListener("change", CelsiusToFahrenheit);
+ 
+function FahrenheitToCelsius() {
+    Fn=document.getElementById("fValue");
+    C = (cN.value - 32) / 1.8;
+    Fn.value=C;
+    //return C;   
 }
+
+cN=document.getElementById("cValue");
+cN.addEventListener("change", FahrenheitToCelsius);
